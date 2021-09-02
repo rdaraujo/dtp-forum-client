f8058f22b805d2aac624c58b702573253a0b9a2c
= <=>
2021-09-01 23:01:56 -0300

    Add Delete button and apply some style

--- src/App.js
+++ src/App.js
@@ -1,42 +1,50 @@
-import React from 'react';
+import { Component as comp, Fragment as frag } from 'react';
+import "./assets/index.css";
 
-const posts = [
-  {
-    titulo: 'Título do Post 1',
-    resumo: 'Resumo do Post 1',
-  },
-  {
-    titulo: 'Título do Post 2',
-    resumo: 'Resumo do Post 2',
-  },
-  {
-    titulo: 'Título do Post 3',
-    resumo: 'Resumo do Post 3',
-  },
-  {
-    titulo: 'Título do Post 4',
-    resumo: 'Resumo do Post 4',
-  },
-  {
-    titulo: 'Título do Post 5',
-    resumo: 'Resumo do Post 5',
-  },
-];
+class App extends comp {
+  state = {
+    posts: [
+      { id: 1, titulo: 'Título do Post 1', resumo: 'Resumo do Post 1' },
+      { id: 2, titulo: 'Título do Post 2', resumo: 'Resumo do Post 2' },
+      { id: 3, titulo: 'Título do Post 3', resumo: 'Resumo do Post 3' },
+      { id: 4, titulo: 'Título do Post 4', resumo: 'Resumo do Post 4' },
+      { id: 5, titulo: 'Título do Post 5', resumo: 'Resumo do Post 5' },
+    ],
+  };
+  
+  deletePost = (postId) => {
+    this.setState({
+      posts: this.state.posts.filter((post) => post.id !== postId),
+    });
+  };
 
-const App = () => {
-  return (
-    <React.Fragment>
-      <>
-        {posts.map((post) => (
-          <div>
-            <h5>{post.titulo}</h5>
-            <p>{post.resumo}</p>
-            <hr />
-          </div>
-        ))}
-      </>
-    </React.Fragment>
-  );
-};
+  // deletePost = (postId) => {
+  //   return () => {
+  //     this.setState({
+  //       posts: this.state.posts.filter((post) => post.id !== postId),
+  //     });
+  //   };
+  // }
+
+  render() {
+    return (
+      <frag>
+        <>
+          {this.state.posts.map((post) => (
+            <div key={post.id}>
+              <h3>{post.titulo}</h3>
+              <h6>{post.resumo}</h6>
+              <button className="btn btn-danger"
+                onClick={() => this.deletePost(post.id)}>
+                EXCLUIR
+              </button>
+              <hr />
+            </div>
+          ))}
+        </>
+      </frag>
+    );
+  }
+}
 
 export default App;

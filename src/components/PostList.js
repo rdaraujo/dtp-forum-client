75e0e2f7bbb8499a10257fe14e5b397d2a56daa7
Rafael Dantas <rdaraujo@gmail.com>
2021-09-15 10:49:17 -0300

    More use of Material UI

--- src/components/PostList.js
+++ src/components/PostList.js
@@ -1,19 +1,19 @@
+import { Button, Grid, IconButton } from '@material-ui/core';
+import AccessTimeIcon from '@material-ui/icons/AccessTime';
+import AddCircleIcon from '@material-ui/icons/AddCircle';
+import DeleteIcon from '@material-ui/icons/Delete';
+import EditIcon from '@material-ui/icons/Edit';
+import FaceIcon from '@material-ui/icons/Face';
+import GradeIcon from '@material-ui/icons/Grade';
+import SortIcon from '@material-ui/icons/Sort';
+import SubjectIcon from '@material-ui/icons/Subject';
+import ThumbDownIcon from '@material-ui/icons/ThumbDown';
+import ThumbUpIcon from '@material-ui/icons/ThumbUp';
 import { useState } from 'react';
 import { Link, useLocation } from 'react-router-dom';
 import { Reaction, Sorting } from './constants';
 
-import { Button, Grid, IconButton } from '@material-ui/core';
 
-import ThumbDownIcon from '@material-ui/icons/ThumbDown';
-import ThumbUpIcon from '@material-ui/icons/ThumbUp';
-import AddCircleIcon from '@material-ui/icons/AddCircle';
-import SortIcon from '@material-ui/icons/Sort';
-import DeleteIcon from '@material-ui/icons/Delete';
-import EditIcon from '@material-ui/icons/Edit';
-import WatchLaterIcon from '@material-ui/icons/WatchLater';
-import ChatIcon from '@material-ui/icons/Chat';
-import FaceIcon from '@material-ui/icons/Face';
-import GradeIcon from '@material-ui/icons/Grade';
 
 const PostList = ( { posts, onDelete, onLike, onEdit } ) => {
   const [sortBy, setSortBy] = useState(Sorting.BY_LIKES)
@@ -48,22 +48,35 @@ const PostList = ( { posts, onDelete, onLike, onEdit } ) => {
       {sortedPosts.map((post) => (
         <div key={post.id} className="post">
 
-          <Grid container spacing={1} alignItems="center">
-            <Grid item>
-              <span className="categoria">{post.categoria}</span>
+          <Grid container direction="row" alignItems="center"> 
+            <Grid container item> 
+              <Grid item className="categoria"><span>{post.categoria}</span></Grid>
             </Grid>
-            <Grid item className="dataPostagem">
-              <WatchLaterIcon fontSize="inherit"/>
-            </Grid>
-            <Grid item className="dataPostagem">
-              {new Date(post.timestamp).toLocaleString('pt-BR')}
+            <Grid container item direction="row" alignItems="center" className="dataPostagem"> 
+              <Grid item><AccessTimeIcon fontSize="inherit"/></Grid>
+              <Grid item><span>{new Date(post.timestamp).toLocaleString('pt-BR')}</span></Grid>
             </Grid>
           </Grid>
 
-          <h3><ChatIcon fontSize="inherit"/>{post.titulo}</h3>
-          <h5>{post.corpo}</h5>
-          <h6><FaceIcon fontSize="inherit"/> {post.autor}</h6>
-          <h6><GradeIcon fontSize="inherit"/> Nota: {post.nota}</h6>
+          <Grid container wrap="nowrap" spacing={1} className="titulo" direction="row" alignItems="center">
+            <Grid item><SubjectIcon fontSize="medium"/></Grid>
+            <Grid item><span>{post.titulo}</span></Grid>
+          </Grid>
+
+          <Grid container spacing={1} className="corpo" direction="row" alignItems="center">
+            <Grid item><span>{post.corpo}</span></Grid>
+          </Grid>
+
+          <Grid container spacing={1} className="autor" direction="row" alignItems="center">
+            <Grid item><FaceIcon fontSize="small"/></Grid>
+            <Grid item><span>{post.autor}</span></Grid>
+          </Grid>
+
+          <Grid container spacing={1} className="nota" direction="row" alignItems="center">
+            <Grid item><GradeIcon fontSize="small"/></Grid>
+            <Grid item><span>{post.nota}</span></Grid>
+          </Grid>
+
           <Grid container alignContent="center" justifyContent="flex-start" spacing={1}>
             <IconButton color="primary" component="span" onClick={() => onLike(post.id, Reaction.LIKE)}>
               <ThumbUpIcon fontSize="small"/>

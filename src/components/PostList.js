809e445b9140c832c20c3214a3124e449ce449b8
Rafael Dantas <rdaraujo@gmail.com>
2021-09-22 20:23:26 -0300

    Simpler Post List

--- src/components/PostList.js
+++ src/components/PostList.js
@@ -23,111 +23,68 @@ const PostList = ( { posts, onDelete, onLike, onEdit } ) => {
 
   return posts.length ? (
     <div>
-      <Grid container spacing={1}>
+      <Grid container>
         <Grid item>
           <Button size="small" color="primary" onClick={sort} startIcon={<Sort fontSize="small"/>}>
             { sortBy === Sorting.BY_LIKES ? <span>Menos Curtidos</span> : <span>Mais Curtidos</span> }
           </Button>
         </Grid>
         <Grid item>
-          <Button size="small" color="secondary" component={Link} to={linkNovoPost} startIcon={<AddCircle fontSize="small"/>} >
-            Novo Post
-          </Button>
+          <Button size="small" color="secondary" component={Link} to={linkNovoPost} startIcon={<AddCircle fontSize="small"/>}>Novo Post</Button>
         </Grid>
       </Grid>
       {sortedPosts.map( post => (
         <div key={post.id} className={classes.post}>
-
-          <Grid container spacing={1} direction="row" alignItems="center">
-            <Grid item>
-              <Grid item className={classes.categoria} component={Link} to={`/${post.categoria}`}><span>{post.categoria}</span></Grid>
-            </Grid>
-            <Grid item>
-              <Grid container className={classes.dataPostagem} alignItems="center">
-                <Grid item><WatchLater fontSize="inherit"/></Grid>
-                <Grid item><span>{new Date(post.timestamp).toLocaleString('pt-BR')}</span></Grid>
-              </Grid>
-            </Grid>
+          <Grid container spacing={1} alignItems="center">
+            <Grid item className={classes.categoria} component={Link} to={`/${post.categoria}`}>{post.categoria}</Grid>
+            <Grid item className={classes.dataPostagem}><WatchLater fontSize="inherit"/></Grid>
+            <Grid item xs={10} className={classes.dataPostagem}>{new Date(post.timestamp).toLocaleString('pt-BR')}</Grid>
           </Grid>
 
-          <Grid container wrap="nowrap" spacing={1} className={classes.titulo} direction="row" alignItems="center">
-            <Grid item><Subject fontSize="medium"/></Grid>
-            <Grid item component={Link} to={`/${post.categoria}/${post.id}`}><span>{post.titulo}</span></Grid>
+          <Grid container spacing={1} wrap="nowrap" alignItems="center" className={classes.titulo}>
+            <Grid item component={Link} to={`/${post.categoria}/${post.id}`}><Subject fontSize="small"/></Grid>
+            <Grid item>{post.titulo}</Grid>
           </Grid>
 
-          <Grid container spacing={1} className={classes.autor} direction="row" alignItems="center">
+          <Grid container spacing={2} alignItems="center" className={classes.details}>
             <Grid item><Face fontSize="small"/></Grid>
-            <Grid item><span>{post.autor}</span></Grid>
-          </Grid>
-
-          <Grid container spacing={1} className={classes.nota} direction="row" alignItems="center">
+            <Grid item>{post.autor}</Grid>
             <Grid item><Grade fontSize="small"/></Grid>
-            <Grid item><span>{post.nota}</span></Grid>
-          </Grid>
-
-          <Grid container spacing={1} className={classes.numeroComentarios} direction="row" alignItems="center">
+            <Grid item>{post.nota}</Grid>
             <Grid item><Message fontSize="small"/></Grid>
-            <Grid item><span>{post.numeroComentarios}</span></Grid>
+            <Grid item>{post.numeroComentarios}</Grid>
           </Grid>
 
-          <Grid container alignContent="center" justifyContent="flex-start" spacing={1}>
-            <IconButton color="primary" component="span" onClick={() => onLike(post.id, Reaction.LIKE)}>
-              <ThumbUp fontSize="inherit"/>
-            </IconButton>
-            <IconButton color="primary" component="span" onClick={() => onLike(post.id, Reaction.DISLIKE)}>
-              <ThumbDown fontSize="inherit"/>
-            </IconButton>
-            <IconButton color="primary" component="span" onClick={() => onEdit(post.id)}>
-              <Edit fontSize="inherit"/>
-            </IconButton>
-            <IconButton color="secondary" component="span" onClick={() => onDelete(post.id)}>
-              <Delete fontSize="inherit"/>
-            </IconButton>
+          <Grid container>
+            <IconButton color="primary" size="small" component="span" onClick={() => onLike(post.id, Reaction.LIKE)}><ThumbUp fontSize="small"/></IconButton>
+            <IconButton color="primary" size="small" component="span" onClick={() => onLike(post.id, Reaction.DISLIKE)}><ThumbDown fontSize="small"/></IconButton>
+            <IconButton color="primary" size="small" component="span" onClick={() => onEdit(post.id)}><Edit fontSize="small"/></IconButton>
+            <IconButton color="secondary" size="small" component="span" onClick={() => onDelete(post.id)}><Delete fontSize="small"/></IconButton>
           </Grid>
         </div>
       ))}
     </div>
   ) : (
-    <Typography>Não há postagens.</Typography>
+    <Typography variant="button">Não há postagens.</Typography>
   )
 }
 
 const useStyles = makeStyles( theme => ({
   titulo: {
     fontSize: '14px',
-    margin: '5px 5px 5px auto',
-    fontWeight: '500',
-    paddingTop: 'inherit',
-  },
-  corpo: {
-    fontSize: '13px',
-    fontWeight: 'lighter',
-    paddingTop: '10px',
-    paddingBottom: '10px',
-  },
-  autor: {
-    fontSize: '11px',
-    margin: '2px 2px 2px auto',
     fontWeight: '500',
   },
-  nota: {
+  details: {
     fontSize: '11px',
-    margin: '2px 2px 2px auto',
-    fontWeight: '500',
-  },
-  numeroComentarios: {
-    fontSize: '11px',
-    margin: '2px 2px 2px auto',
     fontWeight: '500',
   },
   categoria: {
-    padding: '3px 8px',
     fontSize: '10px',
     borderRadius: '4px',
-    border: 'none',
     color: '#fff',
     backgroundColor: '#b7b7b7',
     textDecorationStyle: '-moz-none',
+    margin: '5px',
   },
   dataPostagem: {
     padding: '6px auto 6px auto',
@@ -139,8 +96,8 @@ const useStyles = makeStyles( theme => ({
     backgroundColor: '#eef5ff',
     borderRadius: '10px',
     border: 'none',
-    margin: '10px 15px 15px auto',
-    padding: '10px',
+    margin: '5px 15px 5px auto',
+    padding: '8px',
   },
 }))
 
